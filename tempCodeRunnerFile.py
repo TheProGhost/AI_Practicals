2d75bafe34adddc380a70f07ef75aba33a92b438
TheProGhost <95376129+TheProGhost@users.noreply.github.com>
2022-06-15 11:41:44 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-interestedrows_4 = interestedrows_4[interestedrows_4["Pclass"] == 3]
-# interestedrows_4 = interestedrows_4[interestedrows_4["Sex"] == "female"]
-# interestedrows_4 = interestedrows_4[interestedrows_4["Age"] < 12]
